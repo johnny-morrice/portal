4e86bbee7b6d08e9241996d66bd8fe78c50fe34f
John Morrice <john@functorama.com>
2015-11-14 18:06:50 +0000

    Forwarding on the gateway

---
+++ gateway-forward.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+MACHINE=$1
+PORT=$2
+
+if [[ -z $MACHINE ]] || [[ -z $PORT ]]; then
+    echo "Usage: $0 MACHINE PORT" 1>&2
+    exit 1
+fi
+
+ssh -L$PORT:$MACHINE:22 \
+    -N \
+    pebble.functorama.mor
