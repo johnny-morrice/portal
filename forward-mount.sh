84df4205a7940fd6e256dc998c639f9591bd0a48
John Morrice <john@functorama.com>
2015-11-16 12:16:47 +0000

    Forward mount checks arguments

--- forward-mount.sh
+++ forward-mount.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 MACHINE=$1
-sshfs localhost: -p36525 $MACHINE
+PORT=$2
 
+if [[ -z $MACHINE ]] || [[ -z $PORT ]]; then
+    echo "USAGE: $0 MACHINE PORT" 1>&2
+    exit 1
+fi
+
+sshfs localhost: -p$PORT $MACHINE
